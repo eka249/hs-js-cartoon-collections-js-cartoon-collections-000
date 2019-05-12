1d3a03afcbc28becf50f97418bc91ff99b680e33
eka249 <eka249@nyu.edu>
2019-05-12 18:04:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-function dwarfRollCall(dwarves) {
-  var text =
-  for (i=0, i<length.dwarves; i++){
-     text += (i+1) + ". " + dwarves[i] + " "
-   }
-   return text
-}
 
-function summonCaptainPlanet(planeteerCalls){
-}
-
-function longPlaneteerCalls(words) {
-}
-
-function findTheCheese (foods) {
-}
