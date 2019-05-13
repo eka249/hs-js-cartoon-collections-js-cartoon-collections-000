c6f5d9c0538df52d878c2ba765eef08c0272b6ec
Ellyn Anderson <eka249@nyu.edu>
2019-05-13 11:38:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+//accept array of names and return them numbered
 function dwarfRollCall(dwarves) {
   var text= "";
   for (var i=0; i<dwarves.length; i++){
     text += ((i+1) + ". " + dwarves[i] + " ");
    }
    return text
+   //return text.slice(0,-1)
 }
 
+//accept array; capitalize and add ! after all elements
 function summonCaptainPlanet(planeteerCalls){
   for (var i=0; i<planeteerCalls.length; i++){
     planeteerCalls[i]= planeteerCalls[i].toUpperCase() + "!"
@@ -13,7 +16,7 @@ function summonCaptainPlanet(planeteerCalls){
   return planeteerCalls
 }
 
-
+//trigger true/false depending on word length in given array
 function longPlaneteerCalls(words) {
   for (var i=0; i<words.length; i++){
     if (words[i].length >4) {
@@ -23,7 +26,7 @@ function longPlaneteerCalls(words) {
       }
     }
   }
-
+//accept array and determine if a type of cheese is present
 function findTheCheese (foods) {
   var validcheese= ["cheddar", "gouda", "camembert"];
     for (var i=0; i<validcheese.length; i++){
