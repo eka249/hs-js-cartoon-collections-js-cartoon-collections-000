071bb1ffa3c333c6832f6607e606700003edfd22
Ellyn Anderson <eka249@nyu.edu>
2019-05-13 12:41:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //accept array of names and return them numbered
 function dwarfRollCall(dwarves) {
   var text= "";
-  for (var i=0; i<dwarves.length; i++){
+  for (var i=Math.ceil((dwarves.length)/2); i<dwarves.length; i++){
     text += ((i+1) + ". " + dwarves[i] + " ");
    }
    return text
@@ -28,12 +28,24 @@ function longPlaneteerCalls(words) {
   }
 //accept array and determine if a type of cheese is present
 function findTheCheese (foods) {
-  var validcheese= ["cheddar", "gouda", "camembert"];
+  var validcheese= ["cheddar", "gouda", "camembert", "blue cheese"];
     for (var i=0; i<validcheese.length; i++){
       if (foods.indexOf(validcheese[i])> -1){
         return validcheese[i];
       }
     }
   return "no cheese!";
-
 }
+
+//return all words that start with b
+function wordsWithB (words){
+  var x=[];
+  for (var i=0; i<words.length; i++){
+    if (words[i][0].toLowerCase() =='b'){
+      x.push(words[i]);
+    }
+  }
+  return x;
+}
+
+
