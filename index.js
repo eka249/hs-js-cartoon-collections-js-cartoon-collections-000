b4564c603251689280bcb18030b69b6f75a51737
Ellyn Anderson <eka249@nyu.edu>
2019-05-13 00:45:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+function dwarfRollCall(dwarves) {
+  var text= "";
+  for (var i=0; i<dwarves.length; i++){
+    text += ((i+1) + ". " + dwarves[i] + " ");
+   }
+   return text
+}
 
+function summonCaptainPlanet(planeteerCalls){
+  for (var i=0; i<planeteerCalls.length; i++){
+    planeteerCalls[i]= planeteerCalls[i].toUpperCase() + "!"
+  }
+  return planeteerCalls
+}
+
+
+function longPlaneteerCalls(words) {
+  for (var i=0; i<words.length; i++){
+    if (words[i].length >4) {
+      return true}
+      else {
+        return false
+      }
+    }
+  }
+
+function findTheCheese (foods) {
+  var validcheese= ["cheddar", "gouda", "camembert"];
+    for (var i=0; i<validcheese.length; i++){
+      if (foods.indexOf(validcheese[i])> -1){
+        return validcheese[i];
+        break;
+      }
+      else {
+        return "no cheese!";
+      }
+    }
+}
